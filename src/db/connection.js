ff9804ed3ca7a2d1e314ea60fbb1f4385529ffaf
nelson <nlaranjeira01@gmail.com>
2020-08-24 23:22:08 -0300

    Adicionada configuração  para mockar o mongodb com Jest

--- src/db/connection.js
+++ src/db/connection.js
@@ -24,6 +24,7 @@ module.exports = {
         return mongoose.connect(connectionString, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            useCreateIndex: true,
         });
     },
 };
